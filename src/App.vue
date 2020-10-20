8aa719d541cc7aee51a140ceccb4f40225b802df
Logan Wright <lwright.js@gmail.com>
2020-10-20 14:06:05 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div className="header">
+      <h2>Realtime Pointing Poker</h2>
+    </div>
+    <div className="ChatHistory">
+      <h2>Chat History</h2>
+      <p v-for="message in messages" :key="message.data">
+        {{ message.data }}
+      </p>
+    </div>
+    <button v-on:click="sendMessage('7')">7</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { connect, sendMsg } from "./websocket.js";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      messages: [],
+    };
+  },
+  methods: {
+    sendMessage: function (message) {
+      console.log("Hello");
+      sendMsg(message);
+    },
+  },
+  created: function () {
+    connect((msg) => {
+      this.messages = this.messages.concat(msg);
+    });
+  },
+};
 </script>
 
 <style>
@@ -24,5 +44,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header {
+  background-color: #15223b;
+  width: 100%;
+  margin: 0;
+  padding: 10px;
+  color: white;
+
+  h2 {
+    margin: 0;
+    padding: 0;
+  }
+}
+.ChatHistory {
+  background-color: #f7f7f7;
+  margin: 0;
+  padding: 20px;
+  h2 {
+    margin: 0;
+    padding: 0;
+  }
 }
 </style>
